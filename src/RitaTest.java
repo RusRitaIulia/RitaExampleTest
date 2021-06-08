6f6cd79b92c50d4defa9fecaba463bd8579a1455
Rita Iulia Rus <rita.rus@ro.softvision.global>
2021-06-09 00:56:56 +0300

    more exercises

--- src/RitaTest.java
+++ src/RitaTest.java
@@ -1,4 +1,5 @@
-
+import java.util.Scanner;
+import java.io.*;
 
 public class RitaTest {
     public static void checkOddNumbers(int a){
@@ -33,10 +34,8 @@ public class RitaTest {
     }
     public static void sumProductMinMax(int a, int b, int c){
         int sum = 0, prod=1, min, max;
-
         sum=a+b+c;
         prod=a*b*c;
-
         min = a;
         if (b < min) {
             min = b;
@@ -52,9 +51,78 @@ public class RitaTest {
             max = c;
         }
         System.out.println("Sum is: "+sum);
-        System.out.println("Prd is: "+prod);
+        System.out.println("Prod is: "+prod);
         System.out.println("Min is: "+min);
         System.out.println("Max is: "+max);
+    }
+    public static void reverseNumber(){
+        int number=0;
+        int reversenum =0;
+        System.out.println("Input your number and press enter: ");
+        Scanner in = new Scanner(System.in);
+        number = in.nextInt();
+        while( number != 0 )
+        {
+            reversenum = reversenum * 10;
+            reversenum = reversenum + number%10;
+            number = number/10;
+        }
+
+        System.out.println("Reverse number is: "+reversenum);
+    }
+    public static void countVowelsDigits(String str){
+        int vowels = 0, digits = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+                vowels++;
+            if (ch >= '0' && ch <= '9')
+                digits++;
+        }
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Digits: " + digits);
+    }
+    public static void insertElementInArray(){
+        int n, pos, x;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter no. of elements you want in array:");
+        n = s.nextInt();
+        int a[] = new int[n+1];
+        System.out.println("Enter all the elements:");
+        for(int i = 0; i < n; i++)
+        {
+            a[i] = s.nextInt();
+        }
+        System.out.print("Enter the position where you want to insert element:");
+        pos = s.nextInt();
+        System.out.print("Enter the element you want to insert:");
+        x = s.nextInt();
+        for(int i = (n-1); i >= (pos-1); i--)
+        {
+            a[i+1] = a[i];
+        }
+        a[pos-1] = x;
+        System.out.print("After inserting:");
+        for(int i = 0; i < n; i++)
+        {
+            System.out.print(a[i]+",");
+        }
+        System.out.print(a[n]);
+    }
+    public static void bubbleSort(int arr[]){
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j] > arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+        System.out.println("\nThe sorted array is: ");
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
     }
 
     public static void main(String[] args) {
@@ -68,5 +136,10 @@ public class RitaTest {
        else
            System.out.println("False");
         sumProductMinMax(10,15,20);
+        reverseNumber();
+        countVowelsDigits("Rita1234");
+        insertElementInArray();
+        int arr[]={5, 9, 18, 7, 4, 3, 21, 30, 17, 15, 100};
+        bubbleSort(arr);
     }
 }
